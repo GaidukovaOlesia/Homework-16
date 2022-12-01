e4a9a10874b5964aa99197895d17a861d1de7ed5
GaidukovaOlesia <lesyagaydukova@gmail.com>
2022-12-02 01:15:51 +0200

    homework 16

---
+++ script.js
@@ -0,0 +1,24 @@
+
+debugger;
+function makeAdder(x){
+    debugger;
+    let add = x;
+    console.log(add);
+   function makeAdder2(y){
+        debugger;
+        let add2 = x + y;
+        console.log(add2);
+         function makeAdder3 (a){
+            debugger;
+            let add3 = x + y + a;
+            console.log(add3);
+        }
+        makeAdder3(20);
+    }
+    makeAdder2(5);
+}
+makeAdder(3);
+
+
+
+
